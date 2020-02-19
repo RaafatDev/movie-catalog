95b26ac7ca9b7ea1ab694e37560428a6a3e830e2
RaafatDev <raafat.dev@gmail.com>
2020-02-19 22:44:32 +0100

    Updates

---
+++ precache-manifest.92679e23a27c60995f2f1d68904ea690.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d70e486a42278ae6dbde2ccc9ef826",
+    "url": "/movie-catalog/index.html"
+  },
+  {
+    "revision": "2cc5045864831c8cc417",
+    "url": "/movie-catalog/static/css/2.b105f4da.chunk.css"
+  },
+  {
+    "revision": "ce85a2b824f56ca2e6c1",
+    "url": "/movie-catalog/static/css/main.76533914.chunk.css"
+  },
+  {
+    "revision": "2cc5045864831c8cc417",
+    "url": "/movie-catalog/static/js/2.16a6fec0.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/movie-catalog/static/js/2.16a6fec0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce85a2b824f56ca2e6c1",
+    "url": "/movie-catalog/static/js/main.bdd476e9.chunk.js"
+  },
+  {
+    "revision": "aecd3a0ab07f34cc4cc7",
+    "url": "/movie-catalog/static/js/runtime-main.fa54b2b9.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movie-catalog/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movie-catalog/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movie-catalog/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movie-catalog/static/media/slick.f97e3bbf.svg"
+  }
+]);
