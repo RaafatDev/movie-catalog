f0a5b5531e135a1abc7d1213107efccdd8e97511
RaafatDev <raafatdev@gmail.com>
2020-09-11 14:53:02 +0200

    Updates

---
+++ precache-manifest.aa5d97e18ce859c6388a87aeebd802c4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e66a6a4b48d1596ddfbe4c75f43783",
+    "url": "/movie-catalog/index.html"
+  },
+  {
+    "revision": "b1da3f0e51be8956fc06",
+    "url": "/movie-catalog/static/css/2.134bfcc7.chunk.css"
+  },
+  {
+    "revision": "56fcfe40d4328ddfa74a",
+    "url": "/movie-catalog/static/css/main.284a486d.chunk.css"
+  },
+  {
+    "revision": "b1da3f0e51be8956fc06",
+    "url": "/movie-catalog/static/js/2.9aa150ba.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/movie-catalog/static/js/2.9aa150ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fcfe40d4328ddfa74a",
+    "url": "/movie-catalog/static/js/main.f2e2a2a1.chunk.js"
+  },
+  {
+    "revision": "c3518b91e3831888fd47d578690dff84",
+    "url": "/movie-catalog/static/js/main.f2e2a2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34253ef7d8a82895dd28",
+    "url": "/movie-catalog/static/js/runtime-main.368af259.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movie-catalog/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movie-catalog/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movie-catalog/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movie-catalog/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-catalog/precache-manifest.daa3d23f85c789284f4766da54a843e3.js"
+  "/movie-catalog/precache-manifest.aa5d97e18ce859c6388a87aeebd802c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-catalog/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
